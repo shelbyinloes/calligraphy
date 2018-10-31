1bb1ec11060b710c415c25392f9e706830dbca7f
Shelby Inloes <shelbyinloes@gmail.com>
2018-10-31 18:34:35 -0500

    changing widths, just not smootlhy

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ canvas.height = 500;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 50;
+ctx.lineWidth = 10;
 
 let isDrawing = false;
 let lastX = 0;
@@ -16,22 +16,39 @@ let direction = true;
 function draw(e) {
   if(!isDrawing) return; //stop fn from running when they are not moused down
   console.log(e);
-  ctx.strokeStyle = "black";
+  // ctx.strokeStyle = "black";
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
-  [lastX, lastY] = [e.offsetX, e.offsetY]
-  hue++;
-  if(hue >= 360) {hue = 0};
-  if(ctx.lineWidth >= 50 || ctx.lineWidth <= 1){
-    direction = !direction
-  }
-  if(direction){
-    ctx.lineWidth++;
+
+  if((lastX >= e.offsetX) && (lastY >= e.offsetY)){
+    ctx.lineWidth = 20;
+    // ctx.strokeStyle = "blue"
+  }else if((lastX >= e.offsetX) && (lastY <= e.offsetY)){
+    // ctx.strokeStyle = "pink"
+    ctx.lineWidth = 30;
+  }else if((lastX <= e.offsetX) && (lastY <= e.offsetY)){
+    // ctx.strokeStyle = "green"
+    ctx.lineWidth = 15;
+  }else if((lastX <= e.offsetX) && (lastY >= e.offsetY)){
+    // ctx.strokeStyle = "purple"
+    ctx.lineWidth = 10;
   }else{
-    ctx.lineWidth--;
+    ctx.strokeStyle = "black";
   }
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+
+  // if(ctx.lineWidth >= 50 || ctx.lineWidth <= 1){
+  //   direction = !direction
+  // }
+
+  // if(direction){
+  //   ctx.lineWidth++;
+  // }else{
+  //   ctx.lineWidth--;
+  // }
+
 
 }
 
